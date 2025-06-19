7236bd0f7f73bc2b7c5967ec1a6adfaa0fb43810
jimmytwchow <212626943+jimmytwchow@users.noreply.github.com>
2025-06-19 12:39:12 +0800

    Use webpack as a building system and seperate javascript logic into different files.

---
+++ src/index.js
@@ -0,0 +1,80 @@
+import { CinLoader } from "./cinloader.js";
+
+document.getElementById("btn_load").addEventListener("change", (e) => {
+  if (e.target.files.length > 0) {
+    e.target.disabled = true;
+    const cinFile = e.target.files[0];
+    document.getElementById("lbl_btn_load").textContent = "上傳中…";
+    const fileReadStream = cinFile.stream();
+    CinLoader.load(cinFile.name, fileReadStream)
+      .then((cin) => {
+        document.getElementById("lbl_btn_load").textContent = "完成";
+        cin.onKeynamesChange = function (keynames) {
+          document.getElementById("lbl_composing_text").textContent = keynames;
+        };
+        cin.onCurrentCandidatesChange = function (candidates) {
+          // TODO: may have security problem. HTML script injection
+          let innerHTML = "";
+          let selectIndex;
+          for (let i in candidates) {
+            selectIndex = i;
+            if (cin.spaceStyle == 1) {
+              selectIndex--;
+            }
+            innerHTML +=
+              `<span style="display:inline-block; width:1em; text-align:right;">` +
+              `${
+                selectIndex < 0 ? " " : cin.selkey.charAt(selectIndex)
+              }</span><span>${candidates[i]}</span>`;
+          }
+          document.getElementById("lbl_candidate").innerHTML = innerHTML;
+        };
+        cin.onCommit = function (text) {
+          const textAreaEle = document.getElementById("ta_cinotepad");
+          const newSelectionPos = textAreaEle.selectionStart + text.length;
+          const leftText = textAreaEle.value.substring(
+            0,
+            textAreaEle.selectionStart
+          );
+          const rightText = textAreaEle.value.substring(
+            textAreaEle.selectionEnd
+          );
+          textAreaEle.value = leftText + text + rightText;
+          textAreaEle.setSelectionRange(newSelectionPos, newSelectionPos);
+        };
+        document.getElementById("lbl_im_name").textContent = cin.cname + " ";
+        document.getElementById("ta_cinotepad").onbeforeinput = function (e) {
+          if (e.inputType == "insertText") {
+            cin.pushKey(e.data);
+            e.preventDefault();
+          } else if (e.inputType == "deleteContentBackward") {
+            if (cin.deleteContentBackward()) {
+              e.preventDefault();
+            }
+          }
+        };
+        document.getElementById("ta_cinotepad").onkeydown = function (e) {
+          if (e.key == "Escape") {
+            cin.resetKeys();
+            e.preventDefault();
+          } else if (e.key == "PageUp") {
+            if (cin.previousCandidateList()) {
+              e.preventDefault();
+            }
+          } else if (e.key == "PageDown") {
+            if (cin.nextCandidateList()) {
+              e.preventDefault();
+            }
+          }
+        };
+        cin.enable = document.getElementById("checkbox_enable").checked;
+        document.getElementById("checkbox_enable").onchange = function (e) {
+          cin.enable = e.target.checked;
+        };
+      })
+      .catch((error) => {
+        e.target.disabled = false;
+        Promise.reject(error);
+      });
+  }
+});
---
+++ src/jstextdecoderstream.js
@@ -0,0 +1,28 @@
+const tds = {
+  start() {
+    this.decoder = new TextDecoder(this.encoding, this.options);
+  },
+  transform(chunk, controller) {
+    controller.enqueue(this.decoder.decode(chunk, { stream: true }));
+  },
+};
+
+const _jstds_wm = new WeakMap(); /* info holder */
+
+export default class JSTextDecoderStream extends TransformStream {
+  constructor(encoding = "utf-8", { ...options } = {}) {
+    let t = { ...tds, encoding, options };
+
+    super(t);
+    _jstds_wm.set(this, t);
+  }
+  get encoding() {
+    return _jstds_wm.get(this).decoder.encoding;
+  }
+  get fatal() {
+    return _jstds_wm.get(this).decoder.fatal;
+  }
+  get ignoreBOM() {
+    return _jstds_wm.get(this).decoder.ignoreBOM;
+  }
+}
